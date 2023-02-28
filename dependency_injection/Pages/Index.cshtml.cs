25d844743137bcc00937a7f8707a3bb56ffa1995
Yash Bhanderi <yashbhanderi1818@gmail.com>
2023-02-28 12:30:59 +0530

    28th_feb

--- dependency_injection/Pages/Index.cshtml.cs
+++ dependency_injection/Pages/Index.cshtml.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace dependency_injection.Pages;
-
-public class IndexModel : PageModel
+namespace dependency_injection.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<IndexModel> _logger;
 
-    public void OnGet()
-    {
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
 
+        public void OnGet()
+        {
+
+        }
     }
 }
--- dependency_injection/Pages/Privacy.cshtml.cs
+++ dependency_injection/Pages/Privacy.cshtml.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace dependency_injection.Pages;
-
-public class PrivacyModel : PageModel
+namespace dependency_injection.Pages
 {
-    private readonly ILogger<PrivacyModel> _logger;
-
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public class PrivacyModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<PrivacyModel> _logger;
 
-    public void OnGet()
-    {
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
     }
 }
-
