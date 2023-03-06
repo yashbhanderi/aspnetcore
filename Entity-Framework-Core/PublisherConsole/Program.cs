8d0328e5120504c534542dc1d9b6528eb6b6280e
Yash Bhanderi <yashbhanderi1818@gmail.com>
2023-03-06 18:40:02 +0530

    6th_March

---
+++ Entity-Framework-Core/PublisherConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using PublisherData;
+using PublisherDomain;
+
+namespace PublisherConsole {
+	public class Program {
+		public static void AddAuthor() {
+			var author = new Author { FirstName = "Josie", LastName = "Newf" };
+			using var context = new PublisherContext();
+			context.Authors.Add(author);
+			context.SaveChanges();
+		}
+
+		public static void GetAuthors() {
+			using var context = new PublisherContext();
+			var authors = context.Authors.ToList();
+			foreach (var author in authors) {
+				Console.WriteLine(author.FirstName + " " + author.LastName);
+			}
+		}
+
+		public static void Main() {
+
+			Console.WriteLine("Start of the program");
+
+			try {
+				using PublisherContext context = new();
+				context.Database.EnsureCreated();
+			}
+			catch (Exception ex) {
+				Console.WriteLine("EROOOOOOOOOOOOOOOOOOOOOOOOOOORRR!!!!!!!!!!!!!");
+				Console.WriteLine(ex);
+			}
+
+			//GetAuthors();
+			//AddAuthor();
+			//GetAuthors();
+
+		}
+	}
+}
---
+++ Entity-Framework-Core/PublisherData/PublisherContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData {
+	public class PublisherContext : DbContext {
+		public DbSet<Author> Authors { get; set; }
+		public DbSet<Book> Books { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseSqlServer(
+			  "Data Source=DESKTOP-K63766J;Initial Catalog=PublisherDatabase;Integrated Security=True"
+			);
+		}
+
+	}
+}
